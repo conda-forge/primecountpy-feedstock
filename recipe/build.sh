e83eb72fc61e1d7a3732c84557bfc33e34e0c444
Chenxin Zhong <chenxin.zhong@outlook.com>
2026-03-24 17:48:52 +0800

    Fix setup-args syntax in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,4 @@ set -ex
 $PYTHON -m pip install . -vv --no-build-isolation \
     -Csetup-args=--wrap-mode=nofallback \
     -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
-    -Csetup-args=setup-args=--default-library=shared
+    -Csetup-args=--default-library=shared
