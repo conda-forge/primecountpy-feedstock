9a9b5a4ba97efd0f0c0bcab87881f4d1a5093545
Chenxin Zhong <chenxin.zhong@outlook.com>
2026-03-24 17:43:08 +0800

    Fix build.sh to correctly pass MESON_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@ set -ex
 
 $PYTHON -m pip install . -vv --no-build-isolation \
     -Csetup-args=--wrap-mode=nofallback \
-    -Csetup-args=${MESON_ARGS// / -Csetup-args=}
+    -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
     -Csetup-args=setup-args=--default-library=shared
